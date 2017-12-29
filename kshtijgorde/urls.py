90fc9ea978ad65e910c0a81a7d03d59cb016ff3c
chiseledCoder <nikhil.salome@gmail.com>
2017-12-29 11:15:06 +0530

    debub static files updated

--- kshtijgorde/urls.py
+++ kshtijgorde/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.conf import settings
 from django.conf.urls.static import static #for developement servers
 urlpatterns = [
     url(r'^admin/', admin.site.urls),

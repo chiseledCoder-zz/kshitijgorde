df3fc756dd6f0f4fc5837e5a90a5b7a3ffc2adbe
chiseledCoder <nikhil.salome@gmail.com>
2018-01-14 01:09:31 +0530

    aws s3 bug fix

--- kshtijgorde/settings.py
+++ kshtijgorde/settings.py
@@ -160,11 +160,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static_src", "static_root")
 
-MEDIA_URL = '/media/'
+# MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "static_src", "media_root")
 
@@ -259,7 +259,7 @@ DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-AWS_S3_REGION_NAME = 'ap-south-1'
+AWS_S3_REGION_NAME = 'us-east-1'
 
 AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
 
